c515e63d41b206a2ea0144fd55002b26654071fe
repurica <cjmCJM134679@gmail.com>
2024-07-12 01:13:56 +0800

    gitpage update

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+
+git init
+git add -A
+git commit -m 'deploy'
+
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:repurica/unicamp2024.git main:gh-pages
+
+cd -
